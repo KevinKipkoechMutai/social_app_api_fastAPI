76008026b24d289edacd9d8def154771f9d77c19
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2024-01-01 20:30:03 +0300

    fix: edit user schema

--- app/schemas.py
+++ app/schemas.py
@@ -6,10 +6,14 @@ from pydantic.types import conint
 class UserCreate(BaseModel):
     email: EmailStr
     password: str
+    #edit user creation schema to cater for phone number
+    phone_number: str
 
 class UserOut(BaseModel):
     id: int
     email: EmailStr
+    #edit user output schema to cater for phone number
+    phone_number: str
     created_at: datetime
 
     class Config:
