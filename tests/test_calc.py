28986a9b06348cfaf520411cd93d57f93fd43ea9
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2024-01-22 20:31:51 +0300

    basic tests

---
+++ tests/test_calc.py
@@ -0,0 +1,48 @@
+import pytest
+from app.calculations import add, subtract, BankAccount
+
+@pytest.fixture
+def zero_bank_account():
+    return BankAccount()
+
+@pytest.fixture
+def bank_account():
+    return BankAccount(50)
+
+@pytest.mark.parametrize("num1, num2, expected", [
+    (3, 2, 5),
+    (3, 3, 6),
+    (10, 10, 20),
+    (21, 10, 31)
+])
+def test_add(num1, num2, expected):
+    print("Testing add function")
+    assert add(num1, num2) == expected
+
+@pytest.mark.parametrize("num1, num2, expected", [
+    (2, 1, 1),
+    (10, 8, 2),
+    (4, 4, 0),
+    (5, 3, 2)
+])
+def test_subtract(num1, num2, expected):
+    print("Testing subtract function")
+    assert subtract(num1, num2) == expected
+
+def test_bank_set_initial_amount(bank_account):
+    assert bank_account.balance == 50
+
+def test_bank_default_amount(zero_bank_account):
+    assert zero_bank_account.balance == 0 
+
+def test_withdraw(bank_account):
+    bank_account.withdraw(20)
+    assert bank_account.balance == 30
+    
+def test_deposit(bank_account):
+    bank_account.deposit(20)
+    assert bank_account.balance == 70
+
+def test_collect_interest(bank_account):
+    bank_account.collect_interest()
+    assert round(bank_account.balance, 4) == 55
